ac5838d4dd800ff56cf9a244db7e24930ad03792
Jairakn <40512077+Jairakn@users.noreply.github.com>
2018-06-24 22:50:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,180 @@
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link href="css/style.css" type="text/css" rel="stylesheet" >
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<script src="js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  	<script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+
+  	<script>
+	  	$(document).ready(function() {
+	      $('.bxslider').bxSlider({
+	      infiniteLoop: false,
+	        auto: true,
+	        speed: 1000,
+	    hideControlOnEnd: true,controls:true
+	    });
+	     });
+	</script>		
+									  
+			<title>
+					กองพัฒนาคุณภาพชุมชน และทุนมนุษย์
+			</title>
+			<div class="header">
+				<div class="container-fluid">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"> 
+						<div class="row">
+							<div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
+								<div class="row">
+							  	 	<div class="logo">DCHD</div> 
+										<div class="name">
+											<div class="named">กองพัฒนาคุณภาพชุมชน และทุนมนุษย์</div>
+											<div class="name-en">DIVeISION OF COMMUNITY AND HUMAN DEVELOPMENT</div>
+										</div>
+							    </div>
+							</div>		
+							<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+								<div class="row">
+									 	<div class="iconso">
+									 		<ul class="social">
+										 		<a href="#">
+												<div class="icon">
+													<img src="template/img/fb.png">
+												</div>
+												</a>
+												<a href="#">
+													<div class="icon">
+													<img src="template/img/line.png">
+													</div>
+												</a>
+												<a href="#">
+													<div class="icon">
+														<img src="template/img/yt.png">
+													</div>
+												</a>
+										 	</ul>
+										</div>
+									</div>
+								<div class="row">
+									<div class="menu">
+										<nav>
+										<ul>
+											<a href="#">
+												<div class="main">
+													หน้าแรก
+												</div>
+											</a>
+											<a href="#">
+												<div class="main">
+													แนะนำองค์กร
+												</div>
+											</a>
+											<a href="#">
+												<div class="main">
+													บริการของเรา
+												</div>
+											</a>
+											<a href="#">
+												<div class="main">
+													ระบบฐานข้อมูล
+												</div>
+											</a>
+											<a href="#">
+												<div class="main">
+													ติดต่อเรา
+												</div>
+											</a>
+										</ul>
+										</nav>
+								   </div>
+								</div>
+							</div>
+
+						</div>
+			    	</div>
+			    </div>
+			</div>
+
+ </head>
+ <body>
+ 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.js"></script>
+
+ 		<div class="slide">
+ 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"> 
+ 				<div class="row">
+		 			<div class="bxslider">
+						<div class="sp"><img src="template/img/p2.jpg"></div>
+						<div class="sp"><img src="template/img/p2.jpg"></div>
+						<div class="sp"><img src="template/img/p2.jpg"></div>			
+					</div>
+				</div>
+			</div>
+ 		</div>
+ 		<div class="bg-icon">
+	 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+	 			<div class="row" style="text-align: center;">
+						<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+						    <div class="icont">
+						      	<img src="template/img/001.png">
+						        <div class="textbook">
+						        	เสริมสร้างกระบวนการเรียนรู้<br>ตลอดชีวิต
+						        </div>
+						    </div>
+						</div>
+						 <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+						    <div class="icont">
+						      	<img src="template/img/002.png">
+						        <div class="textbook">
+						        	สร้างเครือข่ายความร่วมมือ<br>โดยการพัฒนาพื้นที่
+						        </div>
+						 	</div>
+						 </div>
+						<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+						    <div class="icont">
+						     	<img src="template/img/003.png">
+						        <div class="textbook">
+						        	ถ่ายทอดองค์ความรู้<br>จากมหาวิทยาลัยสู่ชุมชน
+						        </div>
+						  	</div>
+						</div>
+						<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+						    <div class="icont">
+						      	<img src="template/img/004.png">
+						        <div class="textbook">
+						        	สร้างความผูกพัน <br> และความรับผิดชอบต่อสังคม
+						        </div>
+						   </div>
+						</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="padding-left: 25px;padding-right: 25px;margin-top: 30px;">
+	<!-- ข่าวกิจกรรม !-->
+	    <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+	      <div class="news" style="width: 100%;height: 300px;overflow-y: scroll; scrollbar-arrow-color:blue; scrollbar-face-color: #e7e7e7; scrollbar-3dlight-color: #a0a0a0; scrollbar-darkshadow-color:#888888">  
+	        <div class="megaphone" style="display: inline-block;">
+	          	<img src="img/0001.png">
+	        </div>
+	        <div class="msg" style="display: inline-block;">
+	            โทรโข่ง
+	        </div>
+	        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="margin-top: 20px;border-bottom: 1px solid #00BCD4;padding-bottom: 5px;">
+	           <a href="img/Untitled.png" data-fancybox="group">
+	                  <div class= "gallery_img" style="width: 40%;display: inline-block;">
+	                    	<img src="img/Untitled.png" style="width: 100%;">
+	                    	<span class="ico ico-mglass"></span>
+	                  </div>
+	            </a> 
+	            <div class="main" style="display: inline-block;">
+	              	หัวข้อ<br>
+	            <div class="main2" style="display: inline-block;">
+	            </div>      
+	            </div>  
+	                  
+	        </div>
+	        </div>
+	    </div>
+ </body>
